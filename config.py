ed7a458bec503f845e33a86daaeb79fba8ac1f3d
amalhanaja <amalhanaja@gmail.com>
2017-06-01 00:00:24 +0700

    DATABASE HOST CHANGED

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #SQLALCHEMY_DATABASE_URI = "postgresql://<username>:<password>@<host>/<database>"
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:tanggallahir1998@127.0.0.1/DOY"
+SQLALCHEMY_DATABASE_URI = "postgres://szctstngmzcgsx:e053b41a3dab1fc546994eb0ff188a39994efb22e231406f8295f849e73effa8@ec2-54-225-68-71.compute-1.amazonaws.com:5432/d51aqokvn13rlr"
 SQLALCHEMY_TRACK_MODIFICATION = False
 SECRET_KEY = "In1 b4ru yg jenenge k0d3 r@h@si4! dalam engl1sh"
